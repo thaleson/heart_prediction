34be1f4f17a92a5fc7178adaad3e9277248e26b2
felipe thaleson <thaleson19@hotmail.com>
2024-08-02 17:54:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from streamlit_option_menu import option_menu
 
 
 
+
 # Configuração da página principal
-st.set_page_config(page_title="Heart Prediction", page_icon="❤️🔍")
+st.set_page_config(page_title="Predict_cardio", page_icon="❤️🔍")
 
 
 
