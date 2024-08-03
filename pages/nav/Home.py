9f0db0d8d24818ab7709a076663177a06db55596
thaleson <thaleson19@hotmail.com>
2024-08-03 12:41:39 -0300

    Fix: small change on the about page

--- pages/nav/Home.py
+++ pages/nav/Home.py
@@ -9,20 +9,13 @@ def main():
     # Subtítulo
     st.subheader("Olá! Eu sou Thaleson Silva 👋")
 
-    # Texto principal
-    st.write(
-        "Bem-vindo à aplicação de previsão de doença cardíaca. Aqui você pode utilizar nosso modelo "
-        "de aprendizado de máquina para avaliar o risco de doenças cardíacas com base em dados fornecidos. "
-        "Sinta-se à vontade para explorar as diferentes seções da aplicação para obter mais informações e realizar previsões."
-    )
-
     # Adicionando uma seção com animações
     col1, col2 = st.columns(2)
 
     # Carregar animações
-    with open("animaçoes/pagina_inicial1.json") as source:
+    with open("animacoes/anumacoeshosptal/pagina_inicial3.json") as source:
         animacao_1 = json.load(source)
-    with open("animaçoes/pagina_inicial2.json") as source:
+    with open("animacoes/pagina_inicial2.json") as source:
         animacao_2 = json.load(source)
 
     # Conteúdo da coluna 1
@@ -32,7 +25,7 @@ def main():
         st.write("")
         st.write("")
         st.write("")
-        st.markdown("<h5 style='text-align: justify;'>Ao longo do projeto, vou compartilhar os resultados formais, destacando o desempenho e a precisão do modelo, bem como sua aplicabilidade prática no auxílio ao diagnóstico dessa condição. Prepare-se para embarcar em uma jornada fascinante pelo mundo da ciência de dados e inteligência artificial!</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: justify;'>CardioPredictor é um  projeto visa fornecer previsões precisas sobre o risco de doenças cardíacas, ajudando a promover a saúde e o bem-estar. Acompanhe o progresso do projeto e explore as funcionalidades que oferecemos para uma análise detalhada e útil.</h5>", unsafe_allow_html=True)
 
     # Conteúdo da coluna 2
     with col2:
@@ -44,7 +37,7 @@ def main():
         st.write("")
         st.write("")
         st.write("")
-        st.markdown("<h5 style='text-align: justify;'>Seja muito bem-vindo ao projeto de Previsão de Diabetes! Aqui, você terá a oportunidade de acompanhar todas as etapas envolvidas no desenvolvimento deste projeto, desde a concepção até a criação de um modelo de previsão personalizado, adaptado especificamente às características únicas de cada paciente.</h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: justify;'> Aqui, você terá a oportunidade de acompanhar todas as etapas envolvidas no desenvolvimento deste projeto, desde a concepção até a criação de um modelo de previsão personalizado, adaptado especificamente às características únicas de cada paciente.</h5>", unsafe_allow_html=True)
         st_lottie(animacao_2, height=400, width=440)
 
     # Adicionando um aviso
